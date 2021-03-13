e9723f49f20f34d269511e3c39a9ff733f2eddb7
Jon <jonsege@gmail.com>
2021-03-13 18:11:34 -0500

    add controllers

--- main.go
+++ main.go
@@ -3,17 +3,16 @@ package main
 import (
   "net/http"
   "github.com/gin-gonic/gin"
-  "Documents/books/models"
+  "github.com/segej87/bookstore-test/models"
+  "github.com/segej87/bookstore-test/controllers"
 )
 
 func main() {
   r := gin.Default()
-
-  r.GET("/", func(c *gin.Context) {
-    c.JSON(http.StatusOK, gin.H{"data": "hello world"})    
-  })
   
   models.ConnectDatabase()
+  
+  r.GET("/books", controllers.FindBooks)
 
   r.Run()
 }
